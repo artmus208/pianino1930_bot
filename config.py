64a07f34d061aea8d8fb4d2ae3882d269de95767
Artur Mustafin <mustafinrtz.2000@gmail.com>
2025-07-01 15:30:53 +0300

    [dev] Add tg admins in config

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ DB_HOST=environ.get("DB_HOST")
 DB_PORT=environ.get("DB_PORT")
 DB_PASS=environ.get("DB_PASS")
 TG_TOKEN=environ.get("TG_TOKEN")
-
-print(f"{DB_HOST=}")
+TG_ADMINS=list(map(int, environ.get("TG_ADMINS").split(',')))
 
 
