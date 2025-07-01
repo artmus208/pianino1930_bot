00ed0aa39e620d8cd3673dcda536a93b3f1b6ccc
Artur Mustafin <mustafinrtz.2000@gmail.com>
2025-07-02 00:59:50 +0300

    [dev] stable __init__

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,11 +1,29 @@
-from telegram.ext import ApplicationBuilder, CallbackQueryHandler
+from telegram.ext import (
+    ApplicationBuilder, CallbackQueryHandler, CommandHandler
+)
+
+from .notifications import notify_all
+
 from config import TG_TOKEN
 
 app = ApplicationBuilder().token(TG_TOKEN).build()
 
-from survey import conv_handler
+from .survey import conv_handler
 
 from .collbacks import consent_callback
 
 app.add_handler(conv_handler)
 app.add_handler(CallbackQueryHandler(consent_callback, pattern="^consent_"))
+app.add_handler(CommandHandler("notify", notify_all))
+
+
+from .admin_panel import panel_conv, selection_callback
+
+app.add_handler(panel_conv)
+app.add_handler(
+    CallbackQueryHandler(
+        selection_callback,
+        pattern="^(select_|finish_selection|next_page)" 
+    )
+)
+
