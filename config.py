5c1f438bc534fada59c8e76c2cd6e14b31e6b2a0
Artur Mustafin <mustafinrtz.2000@gmail.com>
2025-07-03 08:43:24 +0300

    [chore] prod env support add

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from os import environ
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv("./envs/artmus_bot.env")
 
 DB_NAME=environ.get("DB_NAME")
 DB_USER=environ.get("DB_USER")
@@ -9,6 +9,9 @@ DB_PORT=environ.get("DB_PORT")
 DB_PASS=environ.get("DB_PASS")
 TG_TOKEN=environ.get("TG_TOKEN")
 TG_ADMINS=list(map(int, environ.get("TG_ADMINS").split(',')))
+GOOGLE_CREDITINALS_JSON=environ.get("GOOGLE_CREDITINALS_JSON")
+GOOGLE_SPREADSHEET_NAME=environ.get("GOOGLE_SPREADSHEET_NAME")
 
 print(f"{DB_HOST=}")
 print(f"{TG_ADMINS=}")
+print(f"{GOOGLE_CREDITINALS_JSON=}")
--- sheets.py
+++ sheets.py
@@ -1,15 +1,22 @@
 #! sheets.py
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-
+from config import GOOGLE_CREDITINALS_JSON, GOOGLE_SPREADSHEET_NAME
+print(GOOGLE_CREDITINALS_JSON)
+print(GOOGLE_SPREADSHEET_NAME)
 # Подключение к Google Sheets
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-credentials = ServiceAccountCredentials.from_json_keyfile_name("sustained-kit-464408-k7-5ae62e764216.json", scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name(GOOGLE_CREDITINALS_JSON, scope)
 client = gspread.authorize(credentials)
 
 # Открываем таблицу по названию
-spreadsheet = client.open("Участники массовки")
+spreadsheet = client.open(GOOGLE_SPREADSHEET_NAME)
 sheet = spreadsheet.sheet1
 
 def add_participant_to_sheet(name, phone, status, char, consent, time_created):
     sheet.append_row([name, phone, status, char, consent, time_created])
+
+
+
+if __name__ == "__main__":
+    add_participant_to_sheet("Луиза", "+7 917 221 1428", "АМС","Зритель", "TRUE", "02.07.2025")
