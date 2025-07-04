9b23354f1e1633a0f060c992e8017ec74c1cbc58
Artur Mustafin <mustafinrtz.2000@gmail.com>
2025-07-04 14:47:03 +0300

    [dev] Add initialization of tg bot app

--- webhook_server.py
+++ webhook_server.py
@@ -1,10 +1,19 @@
 # webhook_server.py
 from fastapi import FastAPI, Request
 from telegram import Update
-from bot import app as telegram_app  # тут должен быть твой telegram Application
+from bot import app as telegram_app 
 import asyncio
 
 app = FastAPI()
+is_initialized = False  # флаг для инициализации один раз
+
+@app.on_event("startup")
+async def startup():
+    global is_initialized
+    if not is_initialized:
+        await telegram_app.initialize()
+        is_initialized = True
+
 
 @app.post("/webhook")
 async def handle_webhook(request: Request):
