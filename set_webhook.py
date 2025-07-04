0ae079a37f01f1522673ca60b0db84a57b6c1d4c
Artur Mustafin <mustafinrtz.2000@gmail.com>
2025-07-04 14:44:39 +0300

    [chore] typo

--- set_webhook.py
+++ set_webhook.py
@@ -2,7 +2,7 @@
 
 from telegram import Bot
 from config import TG_TOKEN
-WEBHOOK_URL = "https://pianino1930.ru/webhook"
+WEBHOOK_URL = "https://pianio1930.ru/webhook"
 
 async def set_webhook():
     bot = Bot(TG_TOKEN)
